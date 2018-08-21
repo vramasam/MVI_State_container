d80e7fcc7fdeaeeece2d64765c2f575d56ebb022
vivek ramasamy <vivek.ramasamy@honeywell.com>
2018-08-21 16:40:53 +0530

    custom_state_container

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+// View, gets Updated Model as input again
+let view = (m) =>   {
+    let minutes = Math.floor(m.time / 60);
+    let seconds = m.time - (minutes * 60);
+    let secondFormatted = `${seconds < 10 ? '0' : ''}${seconds}`;
+    let handler = (event) => {
+        container.dispatch(m.running ? 'STOP' : 'START');
+    }
+    return <div><p>{minutes}:{secondFormatted}</p>
+    <button onClick={handler}>{m.running ? 'Stop' : 'Start'}</button></div>;
+}
+
+const intents = {
+    TICK: 'TICK',
+    START: 'START',
+    STOP: 'STOP',
+    RESET: 'RESET'
+}
+
+// intent(may be user intent or self intent) , that acts on model.
+const update = ( model = {running : false,
+    time : 0}, intent) =>  {
+    const updates = {
+        'START': (model) => Object.assign(model, {running: true}),
+        'STOP': (model) => Object.assign(model, {running: false}),
+        'TICK': (model) => Object.assign(model, {time: model.time + (model.running ? 
+            1 : 0)})
+    };
+    return (updates[intent] || (() => model)) (model);
+};
+
+const createStore = (reducer) => {
+    let internalState;
+    let handlers = [];
+    return {
+        dispatch:(intent) => {
+            internalState = reducer(internalState, intent);
+            handlers.forEach(h => {h ();});
+        },
+        subscribe:(handler)=>{
+            handlers.push(handler)
+        },
+        getState: () => internalState
+    };
+}
+
+let container = createStore(update);
+
+const render = () => {
+    ReactDOM.render(view(container.getState()), document.getElementById('root'));
+};
+
+container.subscribe(render);
+
+setInterval(() => {
+    container.dispatch('TICK');
+}, 1000)
+
